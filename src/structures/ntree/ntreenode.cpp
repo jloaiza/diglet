f903fc3a868dbc12ff724d8ae9e5979f0d2a50df
Ángel Araya <al.arayaq@gmail.com>
2013-06-02 01:10:23 -0600

    nTree

---
+++ src/structures/ntree/ntreenode.cpp
@@ -0,0 +1,46 @@
+
+#include <string>
+#include <iostream>
+#include "ntreenode.h"
+#include "ifile.h"
+#include "../simplelist/simplelist.h"
+
+
+nTreeNode::nTreeNode(Comparable* pPath, std::string pUser)
+{
+    name = pName;
+    _file = pFile;
+    _children = new SimpleList;
+}
+
+void nTreeNode::addChild(nTreeNode pNode)
+{
+    _children->insert(pNode);
+}
+
+iFile* nTreeNode::getChild(std::string pName)
+{
+	_children->get(pName);
+}
+
+/*
+SimpleList* nTreeNode::getChildrenList(){
+    return _children;
+}
+*/
+
+bool nTreeNode::searchChild(std::string pName)
+{
+    return _children->search(pName);
+}	
+	
+void nTreeNode::eraseChild(std::string pName)
+{
+    /** TODO **/
+}
+
+
+void nTreeNode::print()
+{
+	_file->print();
+}
