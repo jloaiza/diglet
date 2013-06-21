0fffb0879290088e56885bfb9ed57ab511270c8d
Ángel Araya <al.arayaq@gmail.com>
2013-06-21 10:21:56 -0600

    Support for libclientcpp

--- src/bin/test.cpp
+++ src/bin/test.cpp
@@ -1,6 +1,10 @@
 
 #include <iostream>
 
+#include "baseconverter.h"
+#include "byteshandler.h"
+#include "client.h"
+
 int main(int argc, char const *argv[])
 {
   std::cout << "Hello, test" << std::endl;
