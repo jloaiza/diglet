c9cd7398c7f034ba515ffd7f1ab29e7db2f756ab
Ángel Araya <al.arayaq@gmail.com>
2013-06-21 11:59:12 -0600

    LSS support

--- src/bin/test.cpp
+++ src/bin/test.cpp
@@ -3,7 +3,16 @@
 
 #include "baseconverter.h"
 #include "byteshandler.h"
+
 #include "client.h"
+
+#include "info.h"
+#include "lss.h"
+#include "lssconsole.h"
+#include "lssmanager.h"
+#include "lssnetworkhandler.h"
+#include "lssoperations.h"
+#include "lssxml.h"
 
 int main(int argc, char const *argv[])
 {
